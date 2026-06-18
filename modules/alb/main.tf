ced4222ee860350159606684c413fe722142f072
maheshgolla-web <mahesh.golla@ankercloud.com>
2026-06-18 10:10:09 +0530

    Create main.tf

---
+++ modules/alb/main.tf
@@ -0,0 +1,52 @@
+resource "aws_lb" "main" {
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = var.security_group_ids
+  subnets            = var.subnet_ids
+
+  tags = {
+    Name        = "${var.project_name}-alb"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "${var.project_name}-tg"
+  port     = var.target_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    path                = var.health_check_path
+    matcher             = "200"
+  }
+
+  tags = {
+    Name        = "${var.project_name}-tg"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_target_group_attachment" "main" {
+  count            = length(var.instance_ids)
+  target_group_arn = aws_lb_target_group.main.arn
+  target_id        = var.instance_ids[count.index]
+  port             = var.target_port
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
